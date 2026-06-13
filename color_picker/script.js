d5ac6c1a7ea814c06c5bc086198ecba69e0a40de
Bhargavi Kondaka <kondakabhargavi15@gmail.com>
2026-06-13 20:10:21 +0530

    Add files via upload

---
+++ color_picker/script.js
@@ -0,0 +1,23 @@
+function btn1() {
+    let btnColor = document.getElementById("button1").textContent;
+    document.getElementById("colorPickerContainer").style.backgroundColor = btnColor;
+    document.getElementById("selectedColorHexCode").textContent = btnColor;
+}
+
+function btn2() {
+    let btnColor = document.getElementById("button2").textContent;
+    document.getElementById("colorPickerContainer").style.backgroundColor = btnColor;
+    document.getElementById("selectedColorHexCode").textContent = btnColor;
+}
+
+function btn3() {
+    let btnColor = document.getElementById("button3").textContent;
+    document.getElementById("colorPickerContainer").style.backgroundColor = btnColor;
+    document.getElementById("selectedColorHexCode").textContent = btnColor;
+}
+
+function btn4() {
+    let btnColor = document.getElementById("button4").textContent;
+    document.getElementById("colorPickerContainer").style.backgroundColor = btnColor;
+    document.getElementById("selectedColorHexCode").textContent = btnColor;
+}
